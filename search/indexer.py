925a9c1d2e2545f685ff4bd0c0e1d428521d7e19
pheeria <olzhas.askar@deliveryhero.com>
2022-07-10 00:21:05 +0200

    feat: add search

---
+++ search/indexer.py
@@ -0,0 +1,43 @@
+import json
+import os
+from client import search_client
+from elasticsearch.helpers import bulk
+
+current_directory = os.path.dirname(__file__)
+
+def create_index():
+    es = search_client()
+    relative_filepath = '../data/restaurants.json'
+    absolute_filepath = os.path.join(current_directory, relative_filepath)
+    with open(absolute_filepath) as file:
+        settings = json.load(file)
+        es.indices.create(index='restaurants', body=settings)
+
+def delete_index():
+    es = search_client()
+    es.indices.delete(index='restaurants')
+
+def index_restaurants():
+    es = search_client()
+    locations = ['gesundbrunnen', 'ostkreuz', 'südkreuz', 'westkreuz']
+    for location in locations:
+        absolute_path = os.path.join(current_directory, f'../data/{location}.json')
+        documents = []
+        with open(absolute_path) as file:
+            restaurants = json.load(file)
+            sections = restaurants['sections']
+            items = []
+            for section in sections:
+                if section['name'] == 'restaurants-delivering-venues':
+                    items = section['items']
+            for venue in items:
+                documents.append({'_index': 'restaurants', '_id': venue['venue']['id'], '_source': venue})
+        bulk(es, documents)
+        
+
+
+
+if __name__ == '__main__':
+    delete_index()
+    create_index()
+    index_restaurants()
